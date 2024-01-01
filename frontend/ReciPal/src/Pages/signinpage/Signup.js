80832b6b270a3ab38bdd3700d1093ba17f29f0a5
Dina Bader <dina19bader@gmail.com>
2024-01-01 09:02:21 +0200

    added the background image for signup

--- frontend/ReciPal/src/Pages/signinpage/Signup.js
+++ frontend/ReciPal/src/Pages/signinpage/Signup.js
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text,ImageBackground } from 'react-native'
 import React from 'react'
 
 const Signin = () => {
   return (
-    <View>
-      <Text>Signup</Text>
-    </View>
+    <ImageBackground source={require("../../../assets/signup.png")} style={{ flex: 1, width: '100%', height: '100%' }}> 
+      <View>
+      </View>
+    </ImageBackground>
   )
 }
 
