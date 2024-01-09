3fce9d9af571130c5f3a7ec299b062af8a8ef71d
Dina Bader <dina19bader@gmail.com>
2024-01-09 13:55:53 +0200

    Frontend <> Added a Food Preference icon

--- frontend/ReciPal/src/Components/Icons/IconsComp.js
+++ frontend/ReciPal/src/Components/Icons/IconsComp.js
@@ -24,6 +24,16 @@ const IconsComp = () => {
       </View>
       </TouchableOpacity>
 
+      <TouchableOpacity>
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 30, marginTop: 10, marginBottom:30}}>
+        <Image
+          source={require("../../../assets/restaurant.png")}
+          style={{ width: 40, height: 40 }}
+        />
+        <Text style={[common.white, { marginLeft: 20 ,fontSize:25}]}>Restaurant</Text>
+      </View>
+      </TouchableOpacity>
+
     </View>
   )
 }
