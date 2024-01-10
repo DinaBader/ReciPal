90664232f0aac161d4db3594cbc1d8db118200c1
Dina Bader <dina19bader@gmail.com>
2024-01-10 15:12:33 +0200

    Frontend <> Added Button to help navigate

--- frontend/ReciPal/src/Pages/settingspage/setting.js
+++ frontend/ReciPal/src/Pages/settingspage/setting.js
@@ -1,25 +1,33 @@
-import { View, Text,Image } from 'react-native'
+import { View, Text,Image,TouchableOpacity } from 'react-native'
 import React from 'react'
 import common from "../../utils/common"
 import style from "./style"
+import { TouchableOpacity } from 'react-native-gesture-handler'
 const Setting = () => {
   return (
     <View style={common.backgroundColor}>
       <Text style={[common.white, style.settingsTitle]}>Settings</Text>
-      <View style={style.next}>
-        <Text style={[common.white,style.editProfile]}>Edit  Profile</Text>
+
+      <TouchableOpacity>
+        <View style={style.next}>
+          <Text style={[common.white,style.editProfile]}>Edit  Profile</Text>
+          <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <View style={style.next}>
+        <Text style={[common.white,style.languages]}>Languages</Text>
         <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
-      </View>
+        </View>
+      </TouchableOpacity>
 
-      <View style={style.next}>
-      <Text style={[common.white,style.languages]}>Languages</Text>
-      <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
-      </View>
-
-      <View style={style.next}>
-      <Text style={[common.white,style.languages]}>FeedBack  </Text>
-      <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
-      </View>
+      <TouchableOpacity>
+        <View style={style.next}>
+        <Text style={[common.white,style.languages]}>FeedBack  </Text>
+        <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
+        </View>
+      </TouchableOpacity>
 
 
     </View>
