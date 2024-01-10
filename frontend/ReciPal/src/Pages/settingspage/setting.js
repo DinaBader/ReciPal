c07a27c04a7e51f8c011daa4eccd7065aea99f90
Dina Bader <dina19bader@gmail.com>
2024-01-10 15:17:17 +0200

    Frontend <> Made The buttons navigate only

--- frontend/ReciPal/src/Pages/settingspage/setting.js
+++ frontend/ReciPal/src/Pages/settingspage/setting.js
@@ -2,32 +2,31 @@ import { View, Text,Image,TouchableOpacity } from 'react-native'
 import React from 'react'
 import common from "../../utils/common"
 import style from "./style"
-import { TouchableOpacity } from 'react-native-gesture-handler'
 const Setting = () => {
   return (
     <View style={common.backgroundColor}>
       <Text style={[common.white, style.settingsTitle]}>Settings</Text>
 
-      <TouchableOpacity>
         <View style={style.next}>
           <Text style={[common.white,style.editProfile]}>Edit  Profile</Text>
-          <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
+          <TouchableOpacity>
+           <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
+          </TouchableOpacity>
         </View>
-      </TouchableOpacity>
 
-      <TouchableOpacity>
         <View style={style.next}>
         <Text style={[common.white,style.languages]}>Languages</Text>
-        <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
+        <TouchableOpacity>
+          <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
+        </TouchableOpacity>
         </View>
-      </TouchableOpacity>
 
-      <TouchableOpacity>
         <View style={style.next}>
         <Text style={[common.white,style.languages]}>FeedBack  </Text>
-        <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
+        <TouchableOpacity>
+          <Image source={require("../../../assets/right.png")} style={style.backIcon}/>
+        </TouchableOpacity>
         </View>
-      </TouchableOpacity>
 
 
     </View>
