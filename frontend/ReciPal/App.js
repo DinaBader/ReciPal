2c5b560cfef3603d7be6b7f2ed805e6502295745
Dina Bader <dina19bader@gmail.com>
2024-01-10 15:35:34 +0200

    Frontend <> Added route to languages

--- frontend/ReciPal/App.js
+++ frontend/ReciPal/App.js
@@ -12,6 +12,7 @@ import MainContainer from './src/UserNavigation/MainContainer';
 import RecipeDetail from './src/Pages/recipedetailpage/Recipedetail'
 import Settings from "./src/Pages/settingspage/Setting";
 import EditProfile from './src/Pages/editProfilepage/EditProfile'
+import Languages from './src/Pages/languagesPage/Languages';
 
 const Stack = createStackNavigator();
 
@@ -51,6 +52,7 @@ const App = () => {
             <Stack.Screen name="MainContainer" component={MainContainer} options={{ headerShown: false }} />
             <Stack.Screen name="Settings" component={Settings} options={{headerShown:false}} />
             <Stack.Screen name="EditProfile" component={EditProfile} options={{headerShown:false}} />
+            <Stack.Screen name="Languages" component={Languages} options={{headerShown:false}} />
 
           </>
         ) : (
