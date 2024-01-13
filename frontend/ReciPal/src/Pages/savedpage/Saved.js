b517c7389fbb7880108f04ba749ff764abbb5cff
Dina Bader <dina19bader@gmail.com>
2024-01-13 22:01:49 +0200

    Frontend <> Added a Saved title for the saved page

--- frontend/ReciPal/src/Pages/savedpage/Saved.js
+++ frontend/ReciPal/src/Pages/savedpage/Saved.js
@@ -5,8 +5,10 @@ import FoodCard from "../../Components/foodcard/FoodCardComp"
 import styles from "./style"
 const Saved = () => { 
   return ( 
+
+
     <View style={[common.backgroundColor]}>
-        
+        <Text style={[common.white,common.header]}>Saved</Text>
         <View style={[styles.item,styles.background ]}>
             <FoodCard source={require("../../../assets/beefchili.jpeg")} text="Beef chili"/>
             <TouchableOpacity style={[styles.deleteButton,common.center]}>
