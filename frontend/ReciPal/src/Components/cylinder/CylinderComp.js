e5c931f9fe327deb75ddeb4b4e481fea7f7b218c
Dina Bader <dina19bader@gmail.com>
2024-01-14 13:10:34 +0200

    Frontend <> Added a circle for the design of the cylinder

--- frontend/ReciPal/src/Components/cylinder/CylinderComp.js
+++ frontend/ReciPal/src/Components/cylinder/CylinderComp.js
@@ -1,10 +1,12 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import style from "./style"
 const CylinderComp = () => {
   return (
     <View style={style.absoluteContainer}>
       <TouchableOpacity style={style.cylinder}>
+        <Image source={require("../../../assets/new-moon.png")}
+        style={{width:60,height:60,marginLeft:5,marginTop:10}}/> 
       </TouchableOpacity>
     </View>
   )
