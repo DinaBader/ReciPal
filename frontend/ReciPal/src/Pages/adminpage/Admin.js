e8d8070616e3fcc4e76c03a3807b2f976b7ed30f
Dina Bader <dina19bader@gmail.com>
2024-01-14 14:19:21 +0200

    Frontend <> Added a useState for each input and made a function to handle name change

--- frontend/ReciPal/src/Pages/adminpage/Admin.js
+++ frontend/ReciPal/src/Pages/adminpage/Admin.js
@@ -5,9 +5,17 @@ import common from "../../utils/common"
 import style from "./style"
 import Input from "../../Components/Inputs/input"
 const Admin = () => {
-//   const [file, setFile] = useState(null); 
-//   const [error, setError] = useState(null); 
-  
+  const [file, setFile] = useState(null); 
+  const [error, setError] = useState(null); 
+  const [name,setName]=useState("");
+  const [calories,serCalories]=useState("");
+  const [country,setCountry]=useState("");
+  const [totaltime,setTotalTime]=useState("");
+  const [difficulty,setDifficulty]=useState("");
+  const [category,setCategory]=useState("");
+  const [ingredients,setIngredients]=useState([]);
+  const [instructions,setInstructions]=useState([]);
+
 //   const [recipeData, setRecipeData] = useState({
 //     name: '',
 //     calories: '',
@@ -16,30 +24,30 @@ const Admin = () => {
 //     serving: '',
 //     difficulty: '',
 //     category: '',
-//     ingredients: '',
-//     instructions: '',
+//     ingredients: [],
+//     instructions: [],
 //   });
 
 
-// const pickImage = async () => { 
-//   const { status } = await ImagePicker. 
-//       requestMediaLibraryPermissionsAsync(); 
+const pickImage = async () => { 
+  const { status } = await ImagePicker. 
+      requestMediaLibraryPermissionsAsync(); 
 
-//   if (status !== "granted") { 
-//       Alert.alert( 
-//           "Permission Denied", 
-//           `Sorry, we need camera  
-//            roll permission to upload images.` 
-//       ); 
-//   } else { 
-//       const result = 
-//           await ImagePicker.launchImageLibraryAsync(); 
-//       if (!result.cancelled) { 
-//           setFile(result.uri); 
-//           setError(null); 
-//       } 
-//   } 
-// }; 
+  if (status !== "granted") { 
+      Alert.alert( 
+          "Permission Denied", 
+          `Sorry, we need camera  
+           roll permission to upload images.` 
+      ); 
+  } else { 
+      const result = 
+          await ImagePicker.launchImageLibraryAsync(); 
+      if (!result.cancelled) { 
+          setFile(result.uri); 
+          setError(null); 
+      } 
+  } 
+}; 
 
 // const handleUpload = async () => {
 //   try {
@@ -66,7 +74,6 @@ const Admin = () => {
 //     instructions: recipeData.instructions,
 //   };
 
-//   // Make API call for image upload
   
 //   const queryString = new URLSearchParams(imageFormData).toString();
 
@@ -82,16 +89,14 @@ const Admin = () => {
 //     console.log('Image uploaded successfully!');
 //   } else {
 //     console.error('Failed to upload image. Check the backend response.');
-//     return; // Stop execution if image upload fails
+//     return; 
 //   }
 
-//   // Continue with the other data upload...
 // } catch (error) {
 //   console.error('Error uploading image:', error.message);
-//   return; // Stop execution if there's an error
+//   return; 
 // }
 
-//   // Make API call for other data
 //   try {
 //     const otherDataResponse = await fetch('http://192.168.0.100:8000/recipe/addRecipe', {
 //       method: 'POST',
@@ -116,39 +121,30 @@ const Admin = () => {
     <View style={[common.backgroundColor]}>
       <Text style={[style.text]}>Add Recipes</Text>
       <Input placeholder="name"
-              value={recipeData.name}
-              onChangeText={(text)=>setRecipeData({ ...recipeData, name: text })} />
+               />
       <Input placeholder="calories"
-              value={recipeData.calories}
-              onChangeText={(text)=>setRecipeData({...recipeData,calories:text})}/>
+              />
       <Input placeholder="country"
-              value={recipeData.country}
-              onChangeText={(text)=>setRecipeData({...recipeData,country:text})}/>
+              />
       <Input placeholder="total time"
-              value={recipeData.totalTime}
-              onChangeText={(text)=>setRecipeData({...recipeData,totalTime:text})}/>
+              />
       <Input placeholder="serving"
-              value={recipeData.serving}
-              onChangeText={(text)=>setRecipeData({...recipeData,serving:text})}/>
+              />
       <Input placeholder="difficulty"
-              value={recipeData.difficulty}
-              onChangeText={(text)=>setRecipeData({...recipeData,difficulty:text})}/>
+              />
       <Input placeholder="category"
-              value={recipeData.category}
-              onChangeText={(text)=>setRecipeData({...recipeData,category:text})}/>
+              />
       <Input placeholder="ingredients"
-            value={recipeData.ingredients}
-            onChangeText={(text)=>setRecipeData({...recipeData,ingredients:text})}/>
+         />
       <Input placeholder="instructions"
-              value={recipeData.instructions}
-              onChangeText={(text)=>setRecipeData({...recipeData,instructions:text})}/>
+              />
       <TouchableOpacity style={[common.center]} 
                 onPress={pickImage}> 
                 <Text style={[common.yellow_bg]}> 
                     Choose Image 
                 </Text> 
       </TouchableOpacity> 
-      <TouchableOpacity style={[common.center]} onPress={handleUpload}>
+      <TouchableOpacity style={[common.center]} >
         <Text style={[common.yellow_bg]}>Upload Recipe</Text>
       </TouchableOpacity>
             {file ? ( 
