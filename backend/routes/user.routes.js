133275f23934e85aacef8defef034a013c6efe0d
Dina Bader <dina19bader@gmail.com>
2024-01-16 00:26:48 +0200

    Backend <> Added route to get saved recipes function

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { addReward, upload_image, get_user,update_image,saveRecipe,unsaveRecipe } = require("../controllers/user.controllers");
+const { addReward, upload_image, get_user,update_image,saveRecipe,unsaveRecipe,getSavedRecipes } = require("../controllers/user.controllers");
 const { authMiddleware } = require("../middlewares/auth.middleware");
 const { authMiddlewareForUpload } = require("../middlewares/authupload.middleware");
 
@@ -9,6 +9,7 @@ router.post('/upload-image', authMiddlewareForUpload, upload_image);
 router.post('/update_image',authMiddlewareForUpload,update_image);
 router.post('/saveRecipe/:userId/:recipeId',saveRecipe);
 router.post('/unsaveRecipe/:userId/:recipeId',unsaveRecipe);
+router.post('/getSavedRecipes/:userId',getSavedRecipes);
 
 router.get("/get-user", get_user);
 
