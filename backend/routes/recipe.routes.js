1540d8a649711cf991084500fbdadd03f98f1a7f
Dina Bader <dina19bader@gmail.com>
2024-01-15 16:29:10 +0200

    Backend <> Added function to update recipe photos

--- backend/routes/recipe.routes.js
+++ backend/routes/recipe.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {addRecipe,deleteRecipe,searchRecipes,addRecipePhoto,getRecipe}=require("../controllers/recipe.controllers");
+const {addRecipe,deleteRecipe,searchRecipes,addRecipePhoto,getRecipe,update_image}=require("../controllers/recipe.controllers");
 
 router.post('/addRecipe',addRecipe);
 router.delete('/deleteRecipe/:id',deleteRecipe);
 router.get('/searchRecipes',searchRecipes);
 router.post('/addRecipePhoto/:id',addRecipePhoto);
+router.post('/update_image/:id',update_image);
 router.get('/getRecipe',getRecipe);
 module.exports=router;
