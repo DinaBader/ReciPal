b43f97060e670aefa5f08e79d9796ddb9940dd6a
Dina Bader <dina19bader@gmail.com>
2024-01-15 10:33:52 +0200

    Frontend <> Added navigation back in saved page

--- frontend/ReciPal/src/Pages/savedpage/Saved.js
+++ frontend/ReciPal/src/Pages/savedpage/Saved.js
@@ -1,14 +1,21 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity,Image } from 'react-native'
 import React from 'react'
 import common from "../../utils/common"
 import FoodCard from "../../Components/foodcard/FoodCardComp"
 import styles from "./style"
-const Saved = () => { 
+const Saved = ({navigation}) => { 
+    const navigateToSettings=()=>{
+        navigation.goBack();
+    }
   return ( 
 
-
     <View style={[common.backgroundColor]}>
-        <Text style={[common.white,common.header]}>Saved</Text>
+        <View style={[common.title]}>
+        <TouchableOpacity onPress={navigateToSettings}>
+            <Image source={require("../../../assets/back.png")} style={common.back_Icon}/>
+        </TouchableOpacity>
+            <Text style={[common.white,common.header]}>Saved</Text>
+        </View>
         <View style={[styles.item,styles.background ]}>
             <FoodCard source={require("../../../assets/beefchili.jpeg")} text="Beef chili"/>
             <TouchableOpacity style={[styles.deleteButton,common.center]}>
