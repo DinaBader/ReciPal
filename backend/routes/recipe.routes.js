c001bd194a59c23c11a5f1a1c7d7690d933db12b
Dina Bader <dina19bader@gmail.com>
2024-01-15 15:43:14 +0200

    Backend <> Added route to get recipes function

--- backend/routes/recipe.routes.js
+++ backend/routes/recipe.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {addRecipe,deleteRecipe,searchRecipes,addRecipePhoto}=require("../controllers/recipe.controllers");
+const {addRecipe,deleteRecipe,searchRecipes,addRecipePhoto,getRecipe}=require("../controllers/recipe.controllers");
 
 router.post('/addRecipe',addRecipe);
 router.delete('/deleteRecipe/:id',deleteRecipe);
 router.get('/searchRecipes',searchRecipes);
 router.post('/addRecipePhoto/:id',addRecipePhoto);
+router.get('/getRecipe',getRecipe);
 module.exports=router;
