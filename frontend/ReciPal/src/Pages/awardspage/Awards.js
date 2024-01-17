0ca2d6416b78d4efd6125b0fbdb719519a4dc203
Dina Bader <dina19bader@gmail.com>
2024-01-18 01:23:27 +0200

    Frontend <> Added function to retreive userId from local storage

--- frontend/ReciPal/src/Pages/awardspage/Awards.js
+++ frontend/ReciPal/src/Pages/awardspage/Awards.js
@@ -1,8 +1,32 @@
 import { View, Text, ScrollView,TouchableOpacity,Image } from 'react-native'
-import React from 'react'
+import React,{ useEffect, useState } from 'react'
+import axios from "axios";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import common from "../../utils/common"
 import AwardsComp from '../../Components/awards/awards'
 const Awards = ({navigation}) => {
+  const [rewards,setRewards]=useState([]);
+  const [userId, setUserId] = useState(null);
+
+  const _retrieveData = async () => {
+    try {
+      const userString = await AsyncStorage.getItem('user');
+      if (userString !== null) {
+        const user = JSON.parse(userString);
+        const retrievedUserId = user._id;
+        setUserId(retrievedUserId);
+      }
+    } catch (error) {
+      console.error('Error retrieving data:', error);
+    }
+  };
+
+
+  useEffect(()=>{
+    _retrieveData();
+    console.log(userId); 
+  },[])
+
   const navigateBack=()=>{
     navigation.goBack();
   }
