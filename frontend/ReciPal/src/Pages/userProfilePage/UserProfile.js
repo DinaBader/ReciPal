ab3681864b345fb742dfe1b6093235f3f2f7e8c4
Dina Bader <dina19bader@gmail.com>
2024-01-19 13:48:46 +0200

    Frontend <> Added a use state and a function to get user image

--- frontend/ReciPal/src/Pages/userProfilePage/UserProfile.js
+++ frontend/ReciPal/src/Pages/userProfilePage/UserProfile.js
@@ -1,10 +1,17 @@
 import { Image, Text, ScrollView, TouchableOpacity ,View} from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import common from "../../utils/common"
 import style from "./style.js"
 import ProfileComp from "../../Components/UserProfile/UserProfileComp.js"
 import Icons from "../../Components/Icons/IconsComp.js"
 const UserProfile = ({navigation}) => {
+  const [image,setImage]=useState('');
+  const getImage=()=>{
+     
+  }
+  useEffect(()=>{
+     getImage();
+  },[])
   return (
     <ScrollView style={common.backgroundColor}>
         <ProfileComp source={require("../../../assets/default.jpg")} />
