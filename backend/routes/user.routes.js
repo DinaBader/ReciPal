5f08c2507f864b1e3a009c66fc95cecf4b1e57be
Dina Bader <dina19bader@gmail.com>
2024-01-19 13:45:32 +0200

    Backend <> Added route to get user image function

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { addReward, upload_image, get_user,update_image,saveRecipe,unsaveRecipe,getSavedRecipes,editProfile ,getRewards} = require("../controllers/user.controllers");
+const { addReward, upload_image, get_user,update_image,get_userImage,saveRecipe,unsaveRecipe,getSavedRecipes,editProfile ,getRewards} = require("../controllers/user.controllers");
 const { authMiddleware } = require("../middlewares/auth.middleware");
 const { authMiddlewareForUpload } = require("../middlewares/authupload.middleware");
 
@@ -12,6 +12,7 @@ router.post('/unsaveRecipe/:userId/:recipeId',unsaveRecipe);
 router.get('/getSavedRecipes/:userId',getSavedRecipes);
 router.post('/editProfile/:userId',editProfile);
 router.get("/getRewards", authMiddleware,getRewards);
+router.get("/get_userImage",authMiddleware,get_userImage);
 
 router.get("/get-user", get_user);
 
