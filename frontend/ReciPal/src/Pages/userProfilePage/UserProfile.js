6feb48e5757e1f8665dcccd4169eb8001fb983d5
Dina Bader <dina19bader@gmail.com>
2024-01-19 15:19:50 +0200

    Frontend <> Added logic to fetch user image

--- frontend/ReciPal/src/Pages/userProfilePage/UserProfile.js
+++ frontend/ReciPal/src/Pages/userProfilePage/UserProfile.js
@@ -4,13 +4,31 @@ import common from "../../utils/common"
 import style from "./style.js"
 import ProfileComp from "../../Components/UserProfile/UserProfileComp.js"
 import Icons from "../../Components/Icons/IconsComp.js"
+import {BASE_URL} from '@env'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import axios from 'axios'
 const UserProfile = ({navigation}) => {
   const [image,setImage]=useState('');
-  const getImage=()=>{
+
+  const getImage=async()=>{
+    try{
+      const Token = await AsyncStorage.getItem('jwt');
+     const response =axios.get(`${BASE_URL}/reward/get_userImage`,
+     {
+      headers:{
+        'Authorization':`Bearer ${Token}`
+      }
+     })
      
+     const Image=response.data; 
+     setImage(Image)
+    }catch(error){
+      console.log("error fetching image",error);
+    }
   }
   useEffect(()=>{
      getImage();
+     console.log(image);
   },[])
   return (
     <ScrollView style={common.backgroundColor}>
