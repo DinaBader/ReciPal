02e113fa05d5aea224c08791600d50c2c5696650
Dina Bader <dina19bader@gmail.com>
2024-01-20 23:47:50 +0200

    Backend <> Added route to get recipe by ingredients function

--- backend/routes/recipe.routes.js
+++ backend/routes/recipe.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {addRecipe,deleteRecipe,searchRecipes,addRecipePhoto,getRecipe,update_image,getRecipeById}=require("../controllers/recipe.controllers");
+const {addRecipe,deleteRecipe,searchRecipes,addRecipePhoto,getRecipe,update_image,getRecipeById,getRecipeByIngredients}=require("../controllers/recipe.controllers");
 
 router.post('/addRecipe',addRecipe);
 router.delete('/deleteRecipe/:id',deleteRecipe);
@@ -9,4 +9,6 @@ router.post('/addRecipePhoto/:id',addRecipePhoto);
 router.post('/update_image/:id',update_image);
 router.get('/getRecipeById/:recipeId',getRecipeById);
 router.get('/getRecipe',getRecipe);
+router.get('/getRecipeByIngredients',getRecipeByIngredients);
+
 module.exports=router;
