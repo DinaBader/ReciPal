a361addfd0fd82b5a016cdc5a52c4eb43233f6cb
Dina Bader <dina19bader@gmail.com>
2024-01-20 18:45:10 +0200

    Frontend <> Added button to upload image

--- frontend/ReciPal/src/UserNavigation/screens/ImageScreen.js
+++ frontend/ReciPal/src/UserNavigation/screens/ImageScreen.js
@@ -1,13 +1,16 @@
 import { View, TouchableOpacity,Text } from 'react-native'
 import React from 'react'
 import common from '../../utils/common'
-
+import style from "./style"
 const ImageScreen = () => {
 
   return (
     <View style={[common.backgroundColor]}>
+      <View style={[common.title]}>
+          <Text style={[common.white,common.header]}>Suggestion</Text>
+      </View>
       <TouchableOpacity>
-        <Text>Upload image</Text>
+        <Text style={[common.yellow_bg,common.button_w,style.btn,common.bold]}>Upload image</Text>
       </TouchableOpacity>
     </View>
   )
---
+++ frontend/ReciPal/src/UserNavigation/screens/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+const Styles=StyleSheet.create({
+     btn:{
+        marginTop:50,
+        marginLeft:50,
+        width:100,
+        textAlign:'center',
+        borderRadius:20,
+        height:25
+     }
+})
+
+export default Styles;
