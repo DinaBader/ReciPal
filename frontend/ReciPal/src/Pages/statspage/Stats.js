8d0b99083c652b5ff7b32bc731286f5c35553d11
Dina Bader <dina19bader@gmail.com>
2024-01-21 23:47:13 +0200

    Frontend <> Added a title and background color for stats page

--- frontend/ReciPal/src/Pages/statspage/Stats.js
+++ frontend/ReciPal/src/Pages/statspage/Stats.js
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import common from '../../utils/common'
 const Stats = () => {
   return (
-    <View>
-      <Text>stats</Text>
+    <View style={common.backgroundColor}> 
+      <Text style={[common.header,common.white]}>Statistics</Text>
     </View>
   )
 }
