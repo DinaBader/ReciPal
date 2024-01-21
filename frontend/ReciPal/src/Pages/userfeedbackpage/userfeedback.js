5a9fe07743bc3313bb8ee45bb967527b1f86a67e
Dina Bader <dina19bader@gmail.com>
2024-01-21 19:14:30 +0200

    Frontend <> Added function to get feedback

--- frontend/ReciPal/src/Pages/userfeedbackpage/userfeedback.js
+++ frontend/ReciPal/src/Pages/userfeedbackpage/userfeedback.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import common from "../../utils/common"
+import {BASE_URL} from "@env"
 const Userfeedback = () => {
   return (
-    <View>
-      <Text>userfeedback</Text>
+    <View style={[common.backgroundColor]}>
+      <Text style={[common.header,common.white]}>User Feedback</Text>
+
     </View>
   )
 }
