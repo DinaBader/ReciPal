dab0b888f3eddf212a52048657dce531844f17f3
Dina Bader <dina19bader@gmail.com>
2024-01-21 19:45:40 +0200

    Frontend <> Fixed fetching feedbacks

--- frontend/ReciPal/src/Pages/userfeedbackpage/userfeedback.js
+++ frontend/ReciPal/src/Pages/userfeedbackpage/userfeedback.js
@@ -4,14 +4,13 @@ import common from "../../utils/common"
 import axios from 'axios'
 import {BASE_URL} from "@env"
 const Userfeedback = () => {
-  const [feedback,setFeedback]=useState([]);
+  const [feedbacks,setFeedback]=useState([]);
    
   const getFeedback=()=>{
     try{
        axios.get(`${BASE_URL}/review/getReviews`
        ).then((res)=>{
-        const response=res.data;
-        console.log(response); 
+        const response=res.data.reviews;
         setFeedback(response);
        })
     }catch(error){
@@ -26,7 +25,6 @@ const Userfeedback = () => {
   return (
     <View style={[common.backgroundColor]}>
       <Text style={[common.header,common.white]}>User Feedback</Text>
-
     </View>
   )
 }
