9c3f124d43834f85dba7f4c83b96a5a3255d9b21
Dina Bader <dina19bader@gmail.com>
2024-01-21 16:36:25 +0200

    Frontend <> Added icons to bottom navigation

--- frontend/ReciPal/src/Components/bottomnav/style.js
+++ frontend/ReciPal/src/Components/bottomnav/style.js
@@ -2,7 +2,20 @@ import { StyleSheet } from "react-native";
 
 const style=StyleSheet.create({
 
-     
+    navIcons:{ 
+        width:30,
+        height:30, 
+    },
+    container:{
+        display: 'flex',
+        flexDirection: 'row', 
+        // justifyContent: 'space-between', 
+        gap:130,
+        alignItems: 'center',
+    },
+    home:{
+        marginLeft:15
+    }
 })
 
 export default style
