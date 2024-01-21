b09348667c73bdb27acaca2d27ccda886b5cba60
Dina Bader <dina19bader@gmail.com>
2024-01-22 00:17:23 +0200

    Frontend <> Added a pie chart and bottom nav in stats page

--- frontend/ReciPal/src/Pages/statspage/Stats.js
+++ frontend/ReciPal/src/Pages/statspage/Stats.js
@@ -1,10 +1,47 @@
 import { View, Text } from 'react-native'
+import { BarChart } from "react-native-gifted-charts";
 import React from 'react'
 import common from '../../utils/common'
-const Stats = () => {
+import AdminNav  from '../../Components/adminnav/AdminNavComp'
+const Stats = ({navigation}) => {
+    const navigatoHome=()=>{
+        navigation.navigate('AdminPage');
+      }     
+    const navigateAddrecipes=()=>{
+        navigation.navigate('AddRecipePage');
+      }
+      const navigateFeedback=()=>{
+        navigation.navigate('UserFeedbackPage')
+      }
+      const navigateToStats=()=>{
+        navigation.navigate('StatsPage');
+      }
+    const barData = [
+        {value: 230,label: 'Jan',frontColor: '#4ABFF4'},
+        {value: 180,label: 'Feb',frontColor: '#79C3DB'},
+        {value: 195,label: 'Mar',frontColor: '#28B2B3'},
+        {value: 250,label: 'Apr',frontColor: '#4ADDBA'},
+        {value: 320,label: 'May',frontColor: '#91E3E3'},
+        ];
+        
   return (
     <View style={common.backgroundColor}> 
       <Text style={[common.header,common.white]}>Statistics</Text>
+      <BarChart
+            showFractionalValue
+            showYAxisIndices
+            noOfSections={4}
+            maxValue={400}
+            data={barData}
+            isAnimated
+            />
+    <AdminNav onPress1={navigatoHome} onPress2={navigateAddrecipes} onPress3={navigateFeedback} onPress4={navigateToStats}
+     source1={require("../../../assets/home.png")}
+     source2={require("../../../assets/add.png")}
+     source3={require("../../../assets/chat.png")}
+     source4={require("../../../assets/stats.png")}
+     />
+
     </View>
   )
 }
