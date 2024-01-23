cb1cd907496772ee1d52dc808f006190b533fd5c
Dina Bader <dina19bader@gmail.com>
2024-01-23 15:07:59 +0200

    Frontend <> Fetched and displayed number of users

--- frontend/ReciPal/src/Pages/statspage/Stats.js
+++ frontend/ReciPal/src/Pages/statspage/Stats.js
@@ -1,10 +1,24 @@
 import { View, Text } from 'react-native'
 import { PieChart } from "react-native-gifted-charts";
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import common from '../../utils/common'
 import AdminNav  from '../../Components/adminnav/AdminNavComp'
+import axios from 'axios'
+import {BASE_URL} from '@env'
 const Stats = ({navigation}) => {
   const [numberOfUsers,setnumberOfUsers]=useState(0)
+  const getuser=()=>{
+    axios.get(`${BASE_URL}/stats/numberOfUsers`,
+    ).then((res)=>{
+      setnumberOfUsers(res.data.numofUsers);
+    }).catch((error)=>{
+      console.error("Error getting number of users",error);
+    })
+  }
+
+  useEffect(()=>{
+    getuser()
+  },[])
 
     const navigatoHome=()=>{
         navigation.navigate('AdminPage');
@@ -19,7 +33,7 @@ const Stats = ({navigation}) => {
         navigation.navigate('StatsPage');
       }
       const pieData = [
-        {value: 54, color: '#177AD5', text: '54%'},
+        {value: numberOfUsers, color: '#177AD5', text: '54%'},
         {value: 40, color: '#79D2DE', text: '30%'},
         {value: 20, color: '#ED6665', text: '26%'},
     ];
