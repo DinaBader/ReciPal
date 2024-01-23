eeaae29d18d2e4bfed162a6dadfd4b6be7f12a79
Dina Bader <dina19bader@gmail.com>
2024-01-23 14:58:06 +0200

    Backend <> Initialized and implemented logic of number of Users function

--- backend/controllers/stats.controllers.js
+++ backend/controllers/stats.controllers.js
@@ -1,7 +1,15 @@
 const Recipe=require("../models/recipe.model");
 const Users=require("../models/user.model");
 
+const numberOfUsers = async(req,res)=>{
+    try{
+        const numofUsers=await Users.find().count();
+        res.status(200).json({numofUsers})
+    }catch(error){
+        res.status(500).json({"error":error});
+    }
+}
 
 module.exports={
-    
+    numberOfUsers
 }
